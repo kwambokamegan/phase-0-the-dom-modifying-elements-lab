f5af2cd8a7ed0119a261f3ca15c4297265ceb4ab
Megan <megan.nyakina@student.moringaschool.com>
2024-07-04 23:39:23 +0300

    adds dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.textContent = 'Megan is the champion'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
